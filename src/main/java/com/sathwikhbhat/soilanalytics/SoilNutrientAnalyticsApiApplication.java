086a0c37706b14d950cab9b1645d40e1983abd81
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2026-07-02 19:08:15 +0530

    chore: initialize Spring Boot project

---
+++ src/main/java/com/sathwikhbhat/soilanalytics/SoilNutrientAnalyticsApiApplication.java
@@ -0,0 +1,12 @@
+package com.sathwikhbhat.soilanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoilNutrientAnalyticsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoilNutrientAnalyticsApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sathwikhbhat/soilanalytics/SoilNutrientAnalyticsApiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.sathwikhbhat.soilanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoilNutrientAnalyticsApiApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
