d47b94f61eff39e30134aef670bc15457d999bba
Sathwik Hejamady Bhat <sathwikhbhat@gmail.com>
2026-07-13 10:53:59 +0530

    refactor: improve formatting

--- src/main/java/com/sathwikhbhat/soilanalytics/gis/dto/GisMarkerResponse.java
+++ src/main/java/com/sathwikhbhat/soilanalytics/gis/dto/GisMarkerResponse.java
@@ -1,6 +1,3 @@
 package com.sathwikhbhat.soilanalytics.gis.dto;
 
-public record GisMarkerResponse(
-        String sampleId,
-        Double latitude,
-        Double longitude) {}
+public record GisMarkerResponse(String sampleId, Double latitude, Double longitude) {}
